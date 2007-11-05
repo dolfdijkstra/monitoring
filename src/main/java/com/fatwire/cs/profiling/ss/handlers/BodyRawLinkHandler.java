da36332cb5fe0a0528d43910086533560a05815a
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2007-11-05 22:19:30 +0000

    Change logger instantiation to LogFactory.getLog(getClass()).

--- src/main/java/com/fatwire/cs/profiling/ss/handlers/BodyRawLinkHandler.java
+++ src/main/java/com/fatwire/cs/profiling/ss/handlers/BodyRawLinkHandler.java
@@ -3,15 +3,10 @@ package com.fatwire.cs.profiling.ss.handlers;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import com.fatwire.cs.profiling.ss.ResultPage;
 import com.fatwire.cs.profiling.ss.util.SSUriHelper;
 
 public class BodyRawLinkHandler extends AbstractBodyHandler {
-
-    private final Log log = LogFactory.getLog(getClass());
 
     private final Pattern rawLinkPattern = Pattern
             .compile("Satellite\\?.*?[\"']");
