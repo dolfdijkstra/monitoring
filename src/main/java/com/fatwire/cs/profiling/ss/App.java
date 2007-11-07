ff6a4bcce9b4e3a66c04f06aef529cc9ebf13bed
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2007-11-07 16:32:41 +0000

    Refactored creation and lifecycle of ExecutorService so that this service can be reused.

--- src/main/java/com/fatwire/cs/profiling/ss/App.java
+++ src/main/java/com/fatwire/cs/profiling/ss/App.java
@@ -2,9 +2,14 @@ package com.fatwire.cs.profiling.ss;
 
 import java.io.File;
 import java.net.URI;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import com.fatwire.cs.profiling.ss.domain.HostConfig;
 import com.fatwire.cs.profiling.ss.jobs.CommandJob;
+import com.fatwire.cs.profiling.ss.jobs.JobFinishedEvent;
+import com.fatwire.cs.profiling.ss.jobs.JobListener;
+import com.fatwire.cs.profiling.ss.jobs.JobScheduledEvent;
+import com.fatwire.cs.profiling.ss.jobs.JobStartedEvent;
 import com.fatwire.cs.profiling.ss.reporting.ReportingListener;
 import com.fatwire.cs.profiling.ss.reporting.reporters.InnerPageletReporter;
 import com.fatwire.cs.profiling.ss.reporting.reporters.LinkCollectingReporter;
@@ -14,6 +19,7 @@ import com.fatwire.cs.profiling.ss.reporting.reporters.NumberOfInnerPageletsRepo
 import com.fatwire.cs.profiling.ss.reporting.reporters.PageCollectingReporter;
 import com.fatwire.cs.profiling.ss.reporting.reporters.PageCriteriaReporter;
 import com.fatwire.cs.profiling.ss.reporting.reporters.PageRenderTimeReporter;
+import com.fatwire.cs.profiling.ss.reporting.reporters.PageletOnlyReporter;
 import com.fatwire.cs.profiling.ss.reporting.reporters.RootElementReporter;
 import com.fatwire.cs.profiling.ss.reporting.reports.FileReport;
 import com.fatwire.cs.profiling.ss.reporting.reports.ReportCollection;
@@ -28,23 +34,23 @@ public class App {
      */
     public static void main(final String[] args) {
 
-        work(
-                "http://www.fatwire.com/cs/",
-                "/cs/ContentServer?c=Page&cid=1141059098394&p=1141059098394&pagename=FW%2FRenderTheHomePage_US",
-                150);
+        //        work(
+        //                "http://www.fatwire.com/cs/",
+        //                "/cs/ContentServer?c=Page&cid=1141059098394&p=1141059098394&pagename=FW%2FRenderTheHomePage_US",
+        //                15);
 
-        //            .create("http://radium.nl.fatwire.com:8080/cs/"));
-        //
-        //           .addStartUri("/cs/ContentServer?pagename=FSIIWrapper&cid=1118867611403&c=Page&p=1118867611403&childpagename=FirstSiteII/FSIILayout&"
-        //                            + HelperStrings.SS_PAGEDATA_REQUEST + "=true");
+        work("http://radium.nl.fatwire.com:8080/cs/" ,
+                "/cs/ContentServer?pagename=FSIIWrapper&cid=1118867611403&c=Page&p=1118867611403&childpagename=FirstSiteII/FSIILayout",
+                Integer.MAX_VALUE);
 
     }
 
     private static void work(String host, String start, int maxPages) {
 
         final HostConfig hc = App.createHostConfig(URI.create(host));
-
-        final RenderCommand command = new RenderCommand(hc, maxPages);
+        final ThreadPoolExecutor readerPool = new RenderingThreadPool();
+        final RenderCommand command = new RenderCommand(hc, maxPages,
+                readerPool);
         command.addStartUri(start);
 
         final File outputDir = App.getOutputDir();
@@ -65,19 +71,36 @@ public class App {
         reportingListener.addReporter(new RootElementReporter(
                 new StdOutReport()));
         reportingListener.addReporter(new NumberOfInnerPageletsReporter(
-                new WarningLevelLoggingReport("report"),4));
+                new WarningLevelLoggingReport("report"), 4));
         reportingListener.addReporter(new Non200ResponseCodeReporter(
                 new WarningLevelLoggingReport("report")));
         reportingListener.addReporter(new InnerPageletReporter(
                 new StdOutReport()));
-        reportingListener.addReporter(new NestingReporter(
-                new StdOutReport()));
+        reportingListener.addReporter(new NestingReporter(new FileReport(
+                outputDir, "nesting.txt"),10));
+        reportingListener.addReporter(new PageletOnlyReporter(new FileReport(
+                outputDir, "pagelet-only.txt")));
 
-        
         
         command.addListener(reportingListener);
         final CommandJob job = new CommandJob(command);
         job.addListener(reportingListener);
+        job.addListener(new JobListener() {
+
+            public void jobFinished(JobFinishedEvent event) {
+                readerPool.shutdown();
+
+            }
+
+            public void jobScheduled(JobScheduledEvent event) {
+
+            }
+
+            public void jobStarted(JobStartedEvent event) {
+
+            }
+
+        });
         job.schedule();
     }
 
