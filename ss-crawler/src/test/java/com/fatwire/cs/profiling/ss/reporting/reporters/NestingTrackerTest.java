31af56da0ee2a233428f8d09fbb598390df6c43d
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2007-11-08 21:27:06 +0000

    Adding test cases

---
+++ ss-crawler/src/test/java/com/fatwire/cs/profiling/ss/reporting/reporters/NestingTrackerTest.java
@@ -0,0 +1,81 @@
+package com.fatwire.cs.profiling.ss.reporting.reporters;
+
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import com.fatwire.cs.profiling.ss.QueryString;
+import com.fatwire.cs.profiling.ss.ResultPage;
+
+public class NestingTrackerTest extends TestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testAdd() {
+        NestingTracker tracker = new NestingTracker();
+        QueryString qs = new QueryString();
+        qs.addParameter("pagename", "XXX");
+
+        QueryString inner = new QueryString();
+        inner.addParameter("pagename", "YYY");
+        inner.addParameter("c", "Page");
+
+        ResultPage page = new ResultPage(qs);
+        page.addMarker(inner);
+
+        tracker.add(page);
+
+        Set s = tracker.getKeys();
+        assertEquals(1, s.size());
+
+    }
+
+    public void testGetKeys() {
+        NestingTracker tracker = new NestingTracker();
+        QueryString qs = new QueryString();
+        qs.addParameter("pagename", "XXX");
+
+        QueryString inner = new QueryString();
+        inner.addParameter("pagename", "YYY");
+        inner.addParameter("c", "Page");
+
+        ResultPage page = new ResultPage(qs);
+        page.addMarker(inner);
+
+        tracker.add(page);
+
+        Set<QueryString> s = tracker.getKeys();
+        for (QueryString qs2 : s) {
+            assertEquals(qs2, qs);
+        }
+    }
+
+    public void testGetNestingLevel() {
+        NestingTracker tracker = new NestingTracker();
+        QueryString qs = new QueryString();
+        qs.addParameter("pagename", "XXX");
+
+        QueryString inner = new QueryString();
+        inner.addParameter("pagename", "YYY");
+        inner.addParameter("c", "Page");
+
+        ResultPage page = new ResultPage(qs);
+        page.addMarker(inner);
+
+        tracker.add(page);
+
+        Set<QueryString> s = tracker.getKeys();
+        for (QueryString qs2 : s) {
+            System.out.println(qs2);
+            assertEquals(1, tracker.getNestingLevel(qs2));
+        }
+        
+    }
+
+}
