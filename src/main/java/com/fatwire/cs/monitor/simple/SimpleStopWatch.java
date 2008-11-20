7401ed696d02d8f8ab1e74fa68fc2be0fc970bdd
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2008-11-20 13:48:59 +0000

    Adding support for marks

--- src/main/java/com/fatwire/cs/monitor/simple/SimpleStopWatch.java
+++ src/main/java/com/fatwire/cs/monitor/simple/SimpleStopWatch.java
@@ -1,33 +1,49 @@
 package com.fatwire.cs.monitor.simple;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.fatwire.cs.monitor.StopWatch;
 
 public class SimpleStopWatch implements StopWatch {
 
-	private long startTime;
+    private long startTime;
 
-	private long elapsed;
+    private long elapsed;
 
-	public long getTimeElapsedInMillis() {
-		return elapsed;
-	}
+    private final Set<Mark> marks = new HashSet<Mark>();
 
-	public void start() {
-		if (startTime != 0) {
+    public long getTimeElapsedInMillis() {
+        return elapsed;
+    }
+
+    public void start() {
+        if (startTime != 0) {
             throw new IllegalStateException("StopWatch is already started");
         }
-		startTime = System.currentTimeMillis();//.nanoTime();
-		elapsed = 0;
+        startTime = System.currentTimeMillis();//.nanoTime();
+        elapsed = 0;
+        marks.clear();
 
-	}
+    }
 
-	public void stop() {
-		if (startTime == 0) {
+    public void stop() {
+        if (startTime == 0) {
             throw new IllegalStateException("StopWatch is not started");
         }
-		elapsed = System.currentTimeMillis()-startTime;// (System.nanoTime() - startTime) / 1000000L;
-		startTime = 0;
+        elapsed = System.currentTimeMillis() - startTime;// (System.nanoTime() - startTime) / 1000000L;
+        startTime = 0;
 
-	}
+    }
 
+    public Mark[] getMarks() {
+        return marks.toArray(new Mark[marks.size()]);
+    }
+
+    public void mark(String name) {
+        if (startTime == 0) {
+            throw new IllegalStateException("StopWatch is not started");
+        }
+        marks.add(new Mark(name, System.currentTimeMillis() - startTime));
+    }
 }
