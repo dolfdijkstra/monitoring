7df2212a9c56c7faa55a320d2bbcf3afac5f75c5
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2008-11-20 12:50:02 +0000

    Adding a status servlet like apache status module to check on concurrent running servlets

---
+++ servlet/src/main/java/com/fatwire/cs/profiling/concurrent/RequestCounter.java
@@ -0,0 +1,106 @@
+package com.fatwire.cs.profiling.concurrent;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.WeakHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class RequestCounter implements
+        ConcurrencyCounter<HttpServletRequest, RequestInfo> {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    private final AtomicInteger concurrencyCounter = new AtomicInteger();
+
+    private final Map<Thread, RequestInfo> threadMap = new WeakHashMap<Thread, RequestInfo>();
+
+    private final String name;
+
+    private final ThreadLocal<RequestInfo> threadLocal = new ThreadLocal<RequestInfo>() {
+
+        /* (non-Javadoc)
+         * @see java.lang.ThreadLocal#initialValue()
+         */
+        @Override
+        protected RequestInfo initialValue() {
+            RequestInfo i = new RequestInfo(Thread.currentThread());
+            synchronized (threadMap) {
+                threadMap.put(Thread.currentThread(), i);
+            }
+            return i;
+        }
+
+    };
+
+    private final Comparator<RequestInfo> requestInfoComparator = new Comparator<RequestInfo>() {
+
+        public int compare(RequestInfo o1, RequestInfo o2) {
+            return o1.getThreadName().compareTo(o2.getThreadName());
+        }
+
+    };
+
+    public RequestCounter(final String name) {
+        this.name = name;
+    }
+
+    public long getTotalCount() {
+        return counter.get();
+    }
+
+    public int getConcurrencyCount() {
+        return concurrencyCounter.get();
+    }
+
+    public Collection<RequestInfo> getCurrentExecutingOperations() {
+        final Set<RequestInfo> s = new TreeSet<RequestInfo>(
+                requestInfoComparator);
+        RequestInfo[] r;
+        synchronized (threadMap) {
+            r = threadMap.values().toArray(new RequestInfo[threadMap.size()]);
+        }
+        for (RequestInfo info : r) {
+            if (info.isAlive()) {//filter out inactive threads
+                s.add(info);
+            }
+        }
+        return s;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public synchronized void reset() {
+        counter.set(0);
+        concurrencyCounter.set(0);
+        for (final RequestInfo info : threadMap.values()) {
+            info.reset();
+        }
+    }
+
+    public void start(final HttpServletRequest request) {
+        final RequestInfo current = threadLocal.get();
+        current.start(request);
+        concurrencyCounter.incrementAndGet();
+        counter.incrementAndGet();
+
+    }
+
+    public void end(final HttpServletRequest request) {
+        final RequestInfo current = threadLocal.get();
+        if (current.isRunning()) {
+            current.end(request);
+            concurrencyCounter.decrementAndGet();
+        }
+
+    }
+
+}
---
+++ servlet/src/main/java/com/fatwire/cs/profiling/concurrent/RequestInfo.java
@@ -0,0 +1,167 @@
+package com.fatwire.cs.profiling.concurrent;
+
+import java.lang.Thread.State;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class RequestInfo {
+
+    private final String threadName;
+
+    private final WeakReference<Thread> thread;
+
+    private final AtomicLong counter = new AtomicLong();
+
+    private volatile String currentUri;
+
+    private volatile long lastStartTime;
+
+    private volatile long lastNanoStartTime;
+
+    private volatile long lastNanoEndTime;
+
+    private volatile long lastExecutionTime;
+
+    private volatile boolean running = false;
+
+    private volatile String method;
+
+    private volatile String remoteHost;
+
+    /**
+     * @param t
+     */
+    public RequestInfo(final Thread t) {
+        super();
+        this.threadName = t.getName();
+        this.thread = new WeakReference<Thread>(t);
+    }
+
+    public boolean isAlive() {
+        Thread t = thread.get();
+        return t != null && t.isAlive();
+    }
+
+    protected String createUri(final HttpServletRequest request) {
+        final StringBuilder b = new StringBuilder(150);
+        //b.append(request.getMethod());
+        //b.append(' ');
+        b.append(request.getRequestURI());
+        final String q = request.getQueryString();
+        if (q != null) {
+            b.append("?").append(q);
+        }
+        return b.toString();
+    }
+
+    protected void start(HttpServletRequest request) {
+        if (running)
+            return;
+        counter.incrementAndGet();
+        this.lastStartTime = System.currentTimeMillis();
+        this.lastNanoStartTime = System.nanoTime();
+        this.currentUri = createUri(request);
+        this.remoteHost = request.getRemoteHost() + ":"
+                + request.getRemotePort();
+        this.method = request.getMethod();
+        running = true;
+    }
+
+    protected void end(HttpServletRequest request) {
+        if (!running)
+            return;
+        this.lastNanoEndTime = System.nanoTime();
+        this.lastExecutionTime = lastNanoEndTime - lastNanoStartTime;
+        running = false;
+    }
+
+    /**
+     * 
+     * @return execution time in nano seconds
+     */
+
+    public long getExecutionTimeForLastRequest() {
+        if (running) return (System.nanoTime() - lastNanoStartTime);
+        return lastExecutionTime;
+    }
+
+    /**
+     * @return the counter
+     */
+    public AtomicLong getCounter() {
+        return counter;
+    }
+
+    /**
+     * @return the currentUri
+     */
+    public String getCurrentUri() {
+        return currentUri;
+    }
+
+    /**
+     * @return the lastEndTime
+     */
+    public long getLastEndTime() {
+        return lastStartTime + (lastNanoEndTime / 1000000L);
+    }
+
+    /**
+     * @return the lastStartTime
+     */
+    public long getLastStartTime() {
+        return lastStartTime;
+    }
+
+    /**
+     * @return the threadName
+     */
+    public String getThreadName() {
+        return threadName;
+    }
+
+    /**
+     * @return the running
+     */
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void reset() {
+        counter.set(0);
+        currentUri = null;
+        method = null;
+
+        lastStartTime = 0L;
+        lastNanoStartTime = 0L;
+        lastNanoEndTime = 0L;
+        lastExecutionTime = 0L;
+        running = false;
+
+    }
+
+    /**
+     * @return the method
+     */
+    public String getMethod() {
+        return method;
+    }
+
+    /**
+     * @return the remoteHost
+     */
+    public String getRemoteHost() {
+        return remoteHost;
+    }
+
+    public State getThreadState() {
+        final Thread x = thread.get();
+        if (x != null) {
+            return x.getState();
+        } else {
+            return null;
+        }
+    }
+}
