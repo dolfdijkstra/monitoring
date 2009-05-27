0fe4aa9a1daaad8b3709f59cd6487bf2508ed4d9
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2009-05-27 09:26:41 +0000

    Refactored the TimeDebugAppender and split the parser into a seperate class, for easier testing.

--- servlet/src/main/java/com/fatwire/cs/profiling/logger/StatisticsAppender.java
+++ servlet/src/main/java/com/fatwire/cs/profiling/logger/StatisticsAppender.java
@@ -4,9 +4,6 @@
 package com.fatwire.cs.profiling.logger;
 
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.regex.MatchResult;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.management.InstanceAlreadyExistsException;
 import javax.management.MBeanRegistrationException;
@@ -19,24 +16,13 @@ import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.helpers.LogLog;
 import org.apache.log4j.spi.LoggingEvent;
 
-class StatisticsAppender extends AppenderSkeleton {
+class StatisticsAppender extends AppenderSkeleton implements TimeDebugParser.ParserCallback {
     static final String TIME_DEBUG = "com.fatwire.logging.cs.time";
 
-    private Pattern pagePattern = Pattern
-            .compile("Execute time  Hours: (\\d{1,}) Minutes: (\\d{1,}) Seconds: (\\d{1,}):(\\d{3})");
+    private TimeDebugParser parser = new TimeDebugParser(this);
 
-    private Pattern elementPattern = create("element", true);
-
-    private Pattern preparedStatementPattern = create("prepared statement",
-            false);
-
-    private Pattern queryPattern = create("query", false); // select,insert,delete,update
-
-    private Pattern updatePattern = create("update statement", true);
-
-    private Pattern queryPatternWithDot = create("query", true);
-
-    private ConcurrentHashMap<String, Stat> stats = new ConcurrentHashMap<String, Stat>();
+    private ConcurrentHashMap<String, Stat> stats = new ConcurrentHashMap<String, Stat>(
+            200, 0.75f, 2); //append is called from asynchronized method
 
     private MBeanServer server;
 
@@ -54,10 +40,9 @@ class StatisticsAppender extends AppenderSkeleton {
             // it's ours
             if (event.getMessage() != null) {
                 try {
-                    parseIt(String.valueOf(event.getMessage()));
+                    parser.parseIt(String.valueOf(event.getMessage()));
                 } catch (Exception e) {
                     LogLog.debug(e.getMessage(), e);
-                    ;
                 }
             }
         }
@@ -65,6 +50,9 @@ class StatisticsAppender extends AppenderSkeleton {
 
     //@Override
     public void close() {
+        if (closed)
+            return;
+        closed = true;
         stats.clear();
         for (Stat s : getStats()) {
             if (s.getName() != null) {
@@ -83,46 +71,8 @@ class StatisticsAppender extends AppenderSkeleton {
         return false;
     }
 
-    private void parseIt(String s) throws Exception {
 
-        long[] pr = this.pageResult(pagePattern.matcher(s));
-        if (pr.length == 1) {
-            update("page", pr[0]);
-            return;
-        }
-        String[] r = result(elementPattern.matcher(s));
-        if (r.length == 2) {
-            update("element", r[0], Long.parseLong(r[1]));
-            return;
-        }
-        r = result(preparedStatementPattern.matcher(s));
-        if (r.length == 2) {
-            update("sql", getStatementType(r[0]), Long.parseLong(r[1]));
-            return;
-
-        }
-        r = result(queryPattern.matcher(s));
-        if (r.length == 2) {
-            update("sql", getStatementType(r[0]), Long.parseLong(r[1]));
-            return;
-
-        }
-        r = result(queryPatternWithDot.matcher(s));
-        if (r.length == 2) {
-            update("sql", getStatementType(r[0]), Long.parseLong(r[1]));
-            return;
-
-        }
-        r = result(updatePattern.matcher(s));
-        if (r.length == 2) {
-            update("sql", getStatementType(r[0]), Long.parseLong(r[1]));
-            return;
-
-        }
-
-    }
-
-    private void update(String type, String subType, long time) {
+    public void update(String type, String subType, long time) {
         if (time > 3722801423L)
             return; // do not log large values do to bug in CS when turning on time debug.
         Stat s = getStat(type, subType);
@@ -136,8 +86,10 @@ class StatisticsAppender extends AppenderSkeleton {
         if (s == null) {
             ObjectName name = null;
             try {
-                name = new ObjectName("com.fatwire.cs:type=StatFromLogger,group="
-                        + type + (subType != null ? ",subType=" + subType : ""));
+                name = new ObjectName(
+                        "com.fatwire.cs:type=StatFromLogger,group="
+                                + type
+                                + (subType != null ? ",subType=" + subType : ""));
             } catch (MalformedObjectNameException e) {
                 LogLog.warn(e.getMessage(), e);
             } catch (NullPointerException e) {
@@ -164,55 +116,7 @@ class StatisticsAppender extends AppenderSkeleton {
         return s;
     }
 
-    private void update(String type, long time) {
-        update(type, null, time);
 
-    }
-
-    private String getStatementType(String s) {
-
-        int t = s == null ? -1 : s.trim().indexOf(" ");
-        if (t != -1) {
-            return s.substring(0, t).toLowerCase();
-        }
-        return "unknown";
-    }
-
-    private Pattern create(String type, boolean dot) {
-        return Pattern.compile("Executed " + type + " (.+?) in (\\d{1,})ms"
-                + (dot ? "." : ""), Pattern.DOTALL);
-    }
-
-    private long[] pageResult(Matcher m) {
-        long[] r = new long[0];
-        if (m.matches()) {
-            MatchResult mr = m.toMatchResult();
-            if (mr.groupCount() == 4) {
-                long t = Long.parseLong(mr.group(1)) * (3600000L);
-                t += Long.parseLong(mr.group(2)) * (60000L);
-                t += Long.parseLong(mr.group(3)) * (1000L);
-                t += Long.parseLong(mr.group(4));
-                r = new long[1];
-                r[0] = t;
-
-            }
-
-        }
-        return r;
-    }
-
-    private String[] result(Matcher m) {
-        String[] r = new String[0];
-        if (m.matches()) {
-            MatchResult mr = m.toMatchResult();
-            r = new String[mr.groupCount()];
-            for (int i = 0; i < mr.groupCount(); i++) {
-                r[i] = mr.group(i + 1);
-
-            }
-        }
-        return r;
-    }
 
     /**
      * @return the server
