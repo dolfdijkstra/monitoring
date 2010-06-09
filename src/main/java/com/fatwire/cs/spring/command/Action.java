0603a62c05942cc9dbb38dfdc503ce126e4c1ecb
Dolf Dijkstra <Dolf.Dijkstra@xs4all.nl>
2010-06-09 15:14:17 +0000

    Stuff that lives in other projects

--- src/main/java/com/fatwire/cs/spring/command/Action.java
+++
@@ -1,9 +0,0 @@
-package com.fatwire.cs.spring.command;
-
-import org.springframework.web.servlet.ModelAndView;
-
-public interface Action {
-    
-    public ModelAndView execute() throws Exception;
-
-}
--- src/main/java/com/fatwire/cs/spring/command/ActionInjcectionStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.fatwire.cs.spring.command;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface ActionInjcectionStrategy {
-    
-    public void inject(Action action, HttpServletRequest request) throws Exception;
-
-}
